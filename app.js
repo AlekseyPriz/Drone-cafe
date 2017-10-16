62528573dea7df526c0ab2a17e5a9be2dd861c6f
AlekseyPriz <aleksey.priz@gmail.com>
2017-10-16 19:59:10 +0300

    Зкоментировал запуск сервера в файле app.js

--- app.js
+++ app.js
@@ -48,8 +48,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, function () {
-  console.log('Сервер Drone Cafe запущен на порту 3000!');
-});
+// app.listen(3000, function () {
+//   console.log('Сервер Drone Cafe запущен на порту 3000!');
+// });
 
 module.exports = app;
